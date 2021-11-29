bf1e610d5e2f13a467d0ec89a884e6222bad6029
Asus <Shomuhammad>
2021-11-29 19:34:58 +0600

    homework16

---
+++ src/com/company/Cow.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Cow {
+    private String sound;
+    private int age;
+    private boolean male;
+
+    public Cow(String sound, int age, boolean male) {
+        this.sound = sound;
+        this.age = age;
+        this.male = male;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Cat cat = new Cat("мяу мяу", 12, true);
+        System.out.println(cat.getSound());
 
+
+        Cow cow = new Cow("муууу муууу",23,true);
+        System.out.println(cow.getSound());
+
+        Dog dog = new Dog("gav gav",10,true);
+        System.out.println(dog.getSound());
     }
 }
